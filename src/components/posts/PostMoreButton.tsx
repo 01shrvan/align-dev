ee49338216470d1befb6d6782772c4dd4db569a3
Shrvan Benke <seemabenke12@gmail.com>
2025-06-17 22:22:10 +0530

    feat: add delete post functionality

---
+++ src/components/posts/PostMoreButton.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import type { PostData } from "@/lib/types"
+import { MoreHorizontal, Trash2 } from "lucide-react"
+import { useState } from "react"
+import { Button } from "../ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
+import DeletePostDialog from "./DeletePostDialog"
+
+interface PostMoreButtonProps {
+  post: PostData
+  className?: string
+}
+
+export default function PostMoreButton({ post, className }: PostMoreButtonProps) {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false)
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon" variant="ghost" className={className}>
+            <MoreHorizontal className="size-5 text-muted-foreground" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            onClick={() => setShowDeleteDialog(true)}
+            className="text-destructive hover:text-destructive focus:text-destructive hover:bg-destructive/10 focus:bg-destructive/10 cursor-pointer"
+          >
+            <Trash2 className="size-4 mr-2" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeletePostDialog post={post} open={showDeleteDialog} onClose={() => setShowDeleteDialog(false)} />
+    </>
+  )
+}
