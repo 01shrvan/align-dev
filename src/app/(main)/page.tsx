08018385f05a5d7ef8536a7c23c2b9f56aac0abd
Shrvan Benke <seemabenke12@gmail.com>
2025-06-18 12:28:03 +0530

    feat: add FollowingFeed component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,14 +1,27 @@
 import PostEditor from "@/components/posts/editor/PostEditor"
 import TrendsSidebar from "@/components/TrendsSidebar"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import ForYouFeed from "./ForYouFeed"
+import FollowingFeed from "./FollowingFeed"
 
 export default function Home() {
   return (
     <main className="flex w-full min-w-0">
       <div className="w-full min-w-0 space-y-5 border-r border-dashed border-border pr-5 mr-5">
         <PostEditor />
-        <div className="border-b border-dashed border-border my-5" />
-        <ForYouFeed />
+        <div className="border-b border-dashed border-border mt-1 mb-2" />
+        <Tabs defaultValue="for-you" className="w-full">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you" className="space-y-4">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following" className="space-y-4">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
