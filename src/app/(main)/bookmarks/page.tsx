706661b6bae54027a4729f154e89597732f31c3f
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-07-29 20:27:06 +0530

    fix: ensure main section has full height in bookmarks page

--- src/app/(main)/bookmarks/page.tsx
+++ src/app/(main)/bookmarks/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function Page() {
     return (
-        <main className="flex w-full min-w-0">
+        <main className="flex w-full min-w-0 min-h-full">
             <div className="w-full min-w-0 space-y-5 border-r border-dashed border-border pr-5 mr-5 min-h-full">
                 <div className="rounded-2xl bg-card p-5 shadow-sm">
                     <h1 className="text-center text-2xl font-bold">Bookmarks</h1>
