d592dd502cd7a96ab8a4b8054d8b301816eadab4
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-08-10 11:03:06 +0530

    refactor: simplify layout structure by removing unnecessary div wrappers

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,24 +6,26 @@ import FollowingFeed from "./FollowingFeed"
 
 export default function Home() {
   return (
-    <main className="flex w-full min-w-0 min-h-full">
-      <div className="w-full min-w-0 space-y-5 border-r border-dashed border-border pr-5 mr-5 min-h-full">
-        <PostEditor />
-        <div className="border-b border-dashed border-border mt-1 mb-2" />
-        <Tabs defaultValue="for-you" className="w-full">
-          <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger value="for-you">For you</TabsTrigger>
-            <TabsTrigger value="following">Following</TabsTrigger>
-          </TabsList>
-          <TabsContent value="for-you" className="space-y-4">
-            <ForYouFeed />
-          </TabsContent>
-          <TabsContent value="following" className="space-y-4">
-            <FollowingFeed />
-          </TabsContent>
-        </Tabs>
-      </div>
-      <TrendsSidebar />
-    </main>
+    <div className="flex-1 pl-5 ml-5 border-l border-dashed border-border">
+      <main className="flex w-full min-w-0 min-h-full">
+        <div className="w-full min-w-0 space-y-5 border-r border-dashed border-border pr-5 mr-5 min-h-full">
+          <PostEditor />
+          <div className="border-b border-dashed border-border mt-1 mb-2" />
+          <Tabs defaultValue="for-you" className="w-full">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="for-you">For you</TabsTrigger>
+              <TabsTrigger value="following">Following</TabsTrigger>
+            </TabsList>
+            <TabsContent value="for-you" className="space-y-4">
+              <ForYouFeed />
+            </TabsContent>
+            <TabsContent value="following" className="space-y-4">
+              <FollowingFeed />
+            </TabsContent>
+          </Tabs>
+        </div>
+        <TrendsSidebar />
+      </main>
+    </div>
   )
 }
